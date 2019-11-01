c3dd2313dd4d0850852800e6a8020c3366b33a22
tshihad <tshihad9@gmail.com>
2019-11-01 22:14:13 +0530

    shihad

--- web/src/components/addMember.jsx
+++ web/src/components/addMember.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 const Form = styled.form`
 width:100%;
+padding:5%;
 background-color:#f7fff6;
 box-shadow: 6px 6px 28px 3px rgba(0,0,0,0.6);`;
 
@@ -14,9 +15,65 @@ export default function Admin(props) {
     })
     return (
         <Form>
-           <TextField id="name"
-        label="Name"
-        type="text"/>
+            <form >
+                <div>
+                    <TextField id="name"
+                        required
+                        label="Name"
+                        margin="normal"
+                        type="text"
+                    />
+                    <TextField id="password"
+                        required
+                        label="Password"
+                        margin="normal"
+                        type="password" />
+                    <br />
+                </div>
+                <div>
+                    <TextField id="house_name"
+                        required
+                        label="House Name"
+                        margin="normal"
+                        type="text" />
+                </div>
+                <div>
+                    <TextField id="phone_no_1"
+                        required
+                        label="Phone no 1"
+                        margin="normal"
+                        type="text" />
+                    <TextField id="phone_no_2"
+                        required
+                        label="Phone no 2"
+                        margin="normal"
+                        type="text" />
+                </div>
+                <div>
+                    <TextField id="office_no"
+                        required
+                        label="Office no"
+                        margin="normal"
+                        type="text" />
+                    <TextField id="uae_no"
+                        required
+                        label="UAE home no"
+                        margin="normal"
+                        type="text" />
+                </div>
+                <div>
+                    <TextField id="email"
+                        required
+                        label="Office no"
+                        margin="normal"
+                        type="email" />
+                    <TextField id="email"
+                        required
+                        label="Office no"
+                        margin="normal"
+                        type="email" />
+                </div>
+            </form>
         </Form>
     )
 }
