ddef1b931aafe78cd9746305ac2f04cbac513a8e
gokulk94 <gokulk@qburst.com>
2019-11-02 16:02:16 +0530

    data for join-majlis

--- web/src/components/sub_components/simple_text_field.jsx
+++ web/src/components/sub_components/simple_text_field.jsx
@@ -1,36 +1,60 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import { createPropertySignature } from 'typescript';
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 200,
-  },
-  dense: {
-    marginTop: 19,
-  },
-  menu: {
-    width: 200,
-  },
-}));
-
-export default function TextFields(props) {
-  const classes = useStyles();
-
+export function TextField(props) {
   return (
-      <TextField
-        id={props.id}
-        label={props.label}
-        className={classes.textField}
+    <div className={props.style}>
+      <label style={{ display: "inline", marginLeft: "2%" }}>{props.placeholder}: </label>
+      <input style={{ display: "inline", marginRight: "2%", float: "right" }} id={props.id}
+        required
         type={props.type}
-        margin="normal"
+        title={props.title}
+        placeholder={props.placeholder}
+        pattern={props.pattern}
       />
-  );
+
+    </div>
+  )
+}
+export function TextField_NoRequired(props){
+  return(
+    <div className={props.style}>
+    <label style={{ display: "inline", marginLeft: "2%" }}>{props.placeholder}: </label>
+    <input style={{ display: "inline", marginRight: "2%", float: "right" }} id={props.id}
+      type={props.type}
+      title={props.title}
+      placeholder={props.placeholder}
+      pattern={props.pattern}
+    />
+
+  </div>
+  )
+}
+export function Select(props) {
+  return (
+    <div className={props.style}>
+      <label>blood group</label>
+      <select name="blood group" id={props.id} style={{ display: "inline", marginRight: "2%", float: "right" }}>
+        <option></option>
+      {props.values.map((option) => (
+        <option value={option}> {option}</option>
+      ))}
+      </select>
+      </div>
+  )
 }
 
+export function Radio(props) {
+  return (
+    <div className={props.style}>
+      <label>{props.label}</label>
+      <div>
+        {props.values.map((option) => (
+          <span>
+            <input required type="radio" name="hi" value={option} /> <span>{option}</span>
+          </span>
+        ))}
+      </div>
+    </div>
+  )
+}
