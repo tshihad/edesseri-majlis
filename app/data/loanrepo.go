dcb00a64db465a3f76c7bbfce0ebbb36c322780d
tshihad <tshihad9@gmail.com>
2019-11-03 18:43:10 +0530

    resoved error

--- app/data/loanrepo.go
+++ app/data/loanrepo.go
@@ -4,6 +4,6 @@ import (
 	"majlis/app/models"
 )
 
-func (r *RepoImp) GetLoan(memberID int) ([]models.Loan, error) {
+func (r *RepoImp) GetLoan(memberID string) ([]models.Loan, error) {
 	return []models.Loan{}, nil
 }
