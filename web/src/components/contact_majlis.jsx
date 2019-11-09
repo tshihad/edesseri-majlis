c7cd3cacb868f3d029e38d9caae58a56de48f646
Ranjith <ranjithr@qburst.com>
2019-11-09 16:01:24 +0530

    contact majlis completed

--- web/src/components/contact_majlis.jsx
+++ web/src/components/contact_majlis.jsx
@@ -1,95 +1,214 @@
-import React, { useEffect } from 'react'
 
-import { makeStyles } from '@material-ui/core/styles';
-import {
-  TextField,
-  Grid,
-  Button,
-  ThemeProvider,
-  createMuiTheme
-} from '@material-ui/core';
-import { green } from '@material-ui/core/colors';
-
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    margin: '3% 8% 2% 10%',
-  },
-  textField: {
-    marginLeft: theme.spacing(0),
-    marginRight: theme.spacing(1),
-    width: '70%',
-    borderColor: "yellow !important",
-  },
-  margin: {
-    margin: theme.spacing(0, 0, 1, 0),
-  },
-}));
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import '../styles/contact.css';
+import styled from 'styled-components';
+import axios from 'axios';
+import PhoneInput from './sub_components/phone_number_input'
 
 
-
-export default function ContactMajlis(props) {
-  const theme = createMuiTheme({
-    palette: {
-      primary: green,
-    },
-  });
-
-  useEffect(() => {
-    props.setState("ContactMajlis")
-  }, [props])
-  const classes = useStyles();
+const Contact = styled.div`
+margin: 10vh 10vw;`;
+export default function Contactmajlis() {
   return (
-    <form>
-      <ThemeProvider theme={theme}>
-        <Grid container spacing={0} className={classes.container}>
-          <Grid item xs={6}>
-            <h4>Contact us</h4>
-            <TextField
-              id="name"
-              required
-              className={classes.textField}
-              label="Name"
-              margin="normal"
-              variant="outlined"
-              color='secondary'
-            />
-          </Grid>
-          <Grid item xs={6}></Grid>
-          <Grid item xs={6}>
-            <TextField
-              required
-              id="email"
-              className={classes.textField}
-              label="Email"
-              margin="normal"
-              variant="outlined"
-              type="email"
-            />
-          </Grid>
-          <Grid item xs={6}></Grid>
-          <Grid item xs={8}>
-            <TextField
-              required
-              id="content"
-              multiline
-              className={classes.textField}
-              label="Content"
-              margin="normal"
-              variant="outlined"
-              rows={6}
-            />
-          </Grid>
-          <Grid item xs={4}></Grid>
-          <Grid item xs={2}>
-            <Button variant="outlined" size="medium" color="primary" className={classes.margin} id="submit">
-              Submit
-          </Button>
-          </Grid>
-        </Grid>
-      </ThemeProvider>
+    <Contact>
+      <div className="head">Contact Majlis</div>
+      <Formik
+        initialValues={{ email: '', phone: '', firstname: '', lastname: '', place: '', country: '', content: '' }}
+        onSubmit={(values, { setSubmitting }) => {
 
-    </form>
+          axios.post('http://10.4.5.22:8080/majlis/contact', {
+            email: values.email,
+            phone: values.phone,
+            fname: values.firstname,
+            lname: values.lastname,
+            place: values.place,
+            country: values.country,
+            content: values.content
+          })
+            .then((response) => {
+              alert(response.statusText);
+            })
+            .catch(function (error) {
+              alert(error);
+            });
+          setSubmitting(false);
+
+        }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string()
+            .email()
+            .required('Required'),
+          firstname: Yup.string()
+            .required('Required'),
+          lastname: Yup.string()
+            .required('Required'),
+          place: Yup.string()
+            .required('Required'),
+
+          country: Yup.string()
+            .required('Required'),
+          content: Yup.string()
+            .required('Required')
+        })}
+      >
+        {props => {
+          const {
+            values,
+            touched,
+            errors,
+            dirty,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            handleReset,
+          } = props;
+          return (
+            <form onSubmit={handleSubmit} className="form">
+              <div>
+                <div className="field" style={{ display: "inline-block" }}>
+                  <label htmlFor="firstname" style={{ display: "inline-block" }}>
+                    First Name                </label>
+                  <input
+                    id="firstname"
+                    placeholder="Enter your place"
+                    type="text"
+                    value={values.firstname}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.firstname && touched.firstname ? 'inputs text-input error' : 'inputs text-input'}
+                  />
+                  {errors.firstname && touched.firstname ? (
+                    <div className="input-feedback">{errors.firstname}</div>
+                  ) : <div className="input-feedback">&nbsp;</div>}
+                </div>
+                <div className="field" style={{ display: "inline-block", marginLeft: "2%" }}>
+                  <label htmlFor="lastname" style={{ display: "inline-block" }}>
+                    Last Name                </label>
+                  <input
+                    id="lastname"
+                    placeholder="Enter your country"
+                    type="text"
+                    value={values.lastname}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.lastname && touched.lastname ? 'inputs text-input error' : 'inputs text-input'}
+                  />
+                  {errors.lastname && touched.lastname ? (
+                    <div className="input-feedback">{errors.lastname}</div>
+                  ) : <div className="input-feedback">&nbsp;</div>}
+                </div>
+              </div>
+              <div>
+                <div className="field" style={{ display: "inline-block" }}>
+                  <label htmlFor="email" style={{ display: "inline-block" }}>
+                    Email
+                </label>
+                  <input
+                    id="email"
+                    placeholder="Enter your email"
+                    type="text"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.email && touched.email ? 'inputs text-input error' : 'inputs text-input'
+                    }
+                  />
+                  {errors.email && touched.email ? (
+                    <div className="input-feedback">{errors.email}</div>)
+                    : <div className="input-feedback">&nbsp;</div>}
+                </div>
+                <div style={{ display: "inline-block", verticalAlign: "top", marginLeft: "3%" }}>
+                  <label htmlFor="phone" style={{ display: "inline-block" }}>
+                    Phone
+                </label>
+                  <PhoneInput id="phone" value={values.lastname}
+                  />
+                  {errors.place && touched.place ? (
+                    <div className="input-feedback">{errors.place}</div>
+                  ) : <div className="input-feedback">&nbsp;</div>}
+                </div>
+              </div>
+              <div>
+                <div className="field" style={{ display: "inline-block" }}>
+                  <label htmlFor="place" style={{ display: "inline-block" }}>
+                    Place                </label>
+                  <input
+                    id="place"
+                    placeholder="Enter your place"
+                    type="text"
+                    value={values.place}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.place && touched.place ? 'inputs text-input error' : 'inputs text-input'}
+                  />
+                  {errors.place && touched.place ? (
+                    <div className="input-feedback">{errors.place}</div>
+                  ) : <div className="input-feedback">&nbsp;</div>}
+                </div>
+                <div className="field" style={{ display: "inline-block", marginLeft: "3%" }}>
+                  <label htmlFor="country" style={{ display: "inline-block" }}>
+                    Country                </label>
+                  <input
+                    id="country"
+                    placeholder="Enter your country"
+                    type="text"
+                    value={values.country}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.country && touched.country ? 'inputs text-input error' : 'inputs text-input'}
+                  />
+                  {errors.country && touched.country ? (
+                    <div className="input-feedback">{errors.country}</div>
+                  ) : <div className="input-feedback">&nbsp;</div>}
+                </div>
+              </div>
+              <div className="field">
+                <label htmlFor="content" style={{ verticalAlign: "top" }}>
+                  content
+                </label>
+                <textarea
+                  id="content"
+                  type="textArea"
+                  rows="8"
+                  cols="83"
+                  placeholder="Type Content Here"
+                  value={values.content}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.content && touched.content ? 'text-input error' : 'text-input'
+                  }
+                />
+                {errors.content && touched.content ? (
+                  <div className="input-feedback">{errors.content}</div>)
+                  : <div className="input-feedback">&nbsp;</div>}
+              </div>
+              <div style={{ textAlign: "center" }}>
+                <button
+                  type="button"
+                  className="buttons outline"
+                  onClick={handleReset}
+                  disabled={!dirty || isSubmitting}
+                >
+                  Reset
+            </button>
+                <button type="submit" className="buttons" disabled={isSubmitting}>
+                  Submit
+            </button>
+              </div>
+            </form>
+          );
+        }}
+      </Formik>
+    </Contact>
+
   )
 }
