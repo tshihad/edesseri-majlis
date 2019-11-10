d8f4ee888c5e51480e933e62ca4c4a12c86421dd
tshihad <tshihad9@gmail.com>
2019-11-10 22:52:35 +0530

    auth handler

---
+++ app/api/authhandler.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"net/http"
+)
+
+func (a *App) handleVerifyAuth(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	m, err := a.VerifyToken(token)
+	if err != nil || m == "" {
+		a.Error("Fail to verify auth")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
