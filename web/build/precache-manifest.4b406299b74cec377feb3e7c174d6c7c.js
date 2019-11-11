1261ca4a84a4e7b7593ad56eb11059b40b40721e
tshihad <tshihad9@gmail.com>
2019-11-11 09:43:40 +0530

    build 11-release

---
+++ web/build/precache-manifest.4b406299b74cec377feb3e7c174d6c7c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6ce852865495825e3fa7c81d702443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc1830cc0d926703ba79",
+    "url": "/static/css/2.1566cbe7.chunk.css"
+  },
+  {
+    "revision": "b4911ed09c6e235ff1cb",
+    "url": "/static/css/main.e1440e30.chunk.css"
+  },
+  {
+    "revision": "bc1830cc0d926703ba79",
+    "url": "/static/js/2.c08543b2.chunk.js"
+  },
+  {
+    "revision": "b4911ed09c6e235ff1cb",
+    "url": "/static/js/main.085f529d.chunk.js"
+  },
+  {
+    "revision": "64ab56e38a1c971639bb",
+    "url": "/static/js/runtime-main.2302c2fb.js"
+  },
+  {
+    "revision": "e354f7fa05ec9777f23088bf844715af",
+    "url": "/static/media/AROMA-Bold.e354f7fa.ttf"
+  },
+  {
+    "revision": "1adad8e69ec6ab4367c3f4fe845a3b55",
+    "url": "/static/media/background.1adad8e6.jpg"
+  },
+  {
+    "revision": "b3bd9525d28b11bbb5c2f54830a9e86d",
+    "url": "/static/media/img1.b3bd9525.jpg"
+  },
+  {
+    "revision": "9796a79689bf0e26629d0a5bfe731f6e",
+    "url": "/static/media/img2.9796a796.jpg"
+  },
+  {
+    "revision": "2efbd2a928ec6b0b454d27ea26bc1ed0",
+    "url": "/static/media/img3.2efbd2a9.jpg"
+  },
+  {
+    "revision": "25ecfd7dc6614721732518f47264f35c",
+    "url": "/static/media/img4.25ecfd7d.JPG"
+  },
+  {
+    "revision": "7f82fb6d5ee98c734b44bc775b506c17",
+    "url": "/static/media/img5.7f82fb6d.jpg"
+  },
+  {
+    "revision": "504d20f1fc403ecc0cd5cdbf43d92530",
+    "url": "/static/media/logo.504d20f1.png"
+  },
+  {
+    "revision": "6a97e257c2b9ede127f05e46e915a440",
+    "url": "/static/media/majlispriority.6a97e257.svg"
+  },
+  {
+    "revision": "688e1456cabe6985247470d89d16b022",
+    "url": "/static/media/member.688e1456.jpg"
+  },
+  {
+    "revision": "7eb9ddf01028f4349944edef23b37288",
+    "url": "/static/media/mission.7eb9ddf0.svg"
+  },
+  {
+    "revision": "56b378e171648fc2b84b34b7fac2f508",
+    "url": "/static/media/riseofmajlis.56b378e1.svg"
+  },
+  {
+    "revision": "68bfd19733388b7216f7d53b1762473f",
+    "url": "/static/media/vision.68bfd197.svg"
+  }
+]);
