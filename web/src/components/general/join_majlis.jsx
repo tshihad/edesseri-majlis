dc8e95ae3c0c595e81a2b1c6ee9574400099d53f
amalk <karayi.amal@gmail.com>
2019-11-12 15:33:12 +0530

    join majlis form partial

--- web/src/components/general/join_majlis.jsx
+++ web/src/components/general/join_majlis.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import Select from 'react-select';
 import styled from 'styled-components'
 import { Formik } from 'formik';
 import * as Yup from 'yup';
@@ -32,6 +33,46 @@ color:#1d4219;
 font-size: 1.5em;
 font-family: 'Comfortaa', cursive;
 `;
+
+const bloodgroupOptions = [
+  {
+      value: 'A+',
+      label: 'A+',
+  },
+  {
+      value: 'B+',
+      label: 'B+',
+  },
+  {
+      value: 'AB+',
+      label: 'AB+',
+  },
+  {
+      value: 'O+',
+      label: 'O+',
+  },
+  {
+      value: 'A-',
+      label: 'A-',
+  },
+  {
+      value: 'B-',
+      label: 'B-',
+  },
+  {
+      value: 'AB-',
+      label: 'AB-',
+  },
+  {
+      value: 'B-',
+      label: 'B-',
+  },
+  {
+      value: 'O-',
+      label: 'O-',
+  },
+];
+
 export default function JoinMajlis(props) {
   useEffect(() => {
     props.setLanButton(false)
@@ -76,7 +117,26 @@ export default function JoinMajlis(props) {
             country: Yup.string()
               .required('Required'),
             content: Yup.string()
-              .required('Required')
+              .required('Required'),
+            phone_number_1: Yup.number()
+              .required('Required'),
+            phone_number_2: Yup.number(),
+            office_phone_number: Yup.number(),
+            home_phone_number: Yup.number(),
+            bloodgroup: Yup.object(),
+            passport: Yup.number()
+            .required('Required'),
+            dob: Yup.date()
+            .required('Required'),
+            job: Yup.date()
+            .required('Required'),
+            company_name: Yup.string()
+            .required('Required'),
+            postcode: Yup.number(),
+            area: Yup.string(),
+            emirates: Yup.string(),
+
+
           })}
         >
           {props => {
@@ -90,7 +150,13 @@ export default function JoinMajlis(props) {
               handleBlur,
               handleSubmit,
               handleReset,
+              setFieldValue
             } = props;
+
+            const onChange = value => {
+              console.log(values);
+              setFieldValue("bloodgroup",value);
+            }
             return (
               <form onSubmit={handleSubmit} className="form">
                 <Grid container spacing={0}>
@@ -123,7 +189,7 @@ export default function JoinMajlis(props) {
                   <Grid container spacing={0} className="field">
                     <Grid item xs={4}>
                       <label htmlFor="housename">
-                        First Name
+                        House Name
                         </label>
                     </Grid>
                     <Grid item xs={8}>
@@ -147,7 +213,7 @@ export default function JoinMajlis(props) {
                   <Grid container spacing={0} className="field">
                     <Grid item xs={4}>
                       <label htmlFor="fathername">
-                        First Name
+                        Father's Name
                         </label>
                     </Grid>
                     <Grid item xs={8}>
@@ -168,11 +234,355 @@ export default function JoinMajlis(props) {
                   </Grid>
                 </Grid>
 
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="name">
+                        Phone Number 1
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="phone_number_1"
+                        placeholder="Enter Your Name"
+                        type="number"
+                        value={values.phone_number_1}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.phone_number_1 && touched.phone_number_1 ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.phone_number_1 && touched.phone_number_1 ? (
+                        <div className="input-feedback">{errors.name}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="housename">
+                        Phone Number 2
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="phone_number_2"
+                        placeholder="Phone number 2"
+                        type="number"
+                        value={values.phone_number_2}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.phone_number_2 && touched.phone_number_2 ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.phone_number_2 && touched.phone_number_2 ? (
+                        <div className="input-feedback">{errors.phone_number_2}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
 
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="housename">
+                        Office Phone Number
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="office_phone_number"
+                        placeholder="Office Phone number"
+                        type="number"
+                        value={values.office_phone_number}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.office_phone_number && touched.office_phone_number ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.office_phone_number && touched.office_phone_number ? (
+                        <div className="input-feedback">{errors.office_phone_number}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
+                  
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="housename">
+                        Home Phone Number
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="home_phone_number"
+                        placeholder="Home Phone number"
+                        type="number"
+                        value={values.home_phone_number}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.home_phone_number && touched.home_phone_number ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.home_phone_number && touched.home_phone_number ? (
+                        <div className="input-feedback">{errors.home_phone_number}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
 
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="housename">
+                         E-mail
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="email"
+                        placeholder="E-mail"
+                        type="string"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.email && touched.email ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.email && touched.email ? (
+                        <div className="input-feedback">{errors.email}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
 
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="bloodgroup" style={{color:'black',fontSize:'16px'}}>
+                         Blood Group
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <Select
+                        id="bloodgroup"
+                        placeholder="Blood Group"
+                        options={bloodgroupOptions}
+                        value={values.bloodgroup}
+                        onChange={onChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.bloodgroup && touched.bloodgroup ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.bloodgroup && touched.bloodgroup ? (
+                        <div className="input-feedback">{errors.bloodgroup}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
 
+                <Grid item xs={12}><Headline>Personal Identification</Headline></Grid>
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="passport">
+                         Passport
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="passport"
+                        placeholder="Passport"
+                        type="number"
+                        value={values.passport}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.passport && touched.passport ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.passport && touched.passport ? (
+                        <div className="input-feedback">{errors.passport}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
 
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="dob">
+                         Date of Birth
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="dob"
+                        placeholder="dob"
+                        type="date"
+                        value={values.dob}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.dob && touched.dob ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.dob && touched.dob ? (
+                        <div className="input-feedback">{errors.dob}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={12}><Headline>Company Information</Headline></Grid>
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="job">
+                         Job
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="job"
+                        placeholder="Job"
+                        type="text"
+                        value={values.job}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.job && touched.job ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.job && touched.job ? (
+                        <div className="input-feedback">{errors.job}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="company_name">
+                         Job
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="company_name"
+                        placeholder="Company Name"
+                        type="text"
+                        value={values.company_name}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.company_name && touched.company_name ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.company_name && touched.company_name ? (
+                        <div className="input-feedback">{errors.company_name}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="postcode">
+                         Job
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="postcode"
+                        placeholder="Postcode"
+                        type="number"
+                        value={values.postcode}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.postcode && touched.postcode ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.postcode && touched.postcode ? (
+                        <div className="input-feedback">{errors.postcode}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="area">
+                         Job
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="area"
+                        placeholder="Area"
+                        type="text"
+                        value={values.area}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.area && touched.area ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.area && touched.area ? (
+                        <div className="input-feedback">{errors.area}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="emirates">
+                         Job
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <input
+                        id="emirates"
+                        placeholder="Emirates"
+                        type="text"
+                        value={values.emirates}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.emirates && touched.emirates ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.emirates && touched.emirates ? (
+                        <div className="input-feedback">{errors.emirates}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <Grid container spacing={0} className="field">
+                    <Grid item xs={4}>
+                      <label htmlFor="institution">
+                         Institution
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <Select
+                        id="institution"
+                        placeholder="Institution"
+                        value={values.institution}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                          errors.institution && touched.institution ? 'inputs text-input error' : 'inputs text-input'}
+                      />
+                      {errors.institution && touched.institution ? (
+                        <div className="input-feedback">{errors.institution}</div>
+                      ) : <div className="input-feedback">&nbsp;</div>}
+                    </Grid>
+                  </Grid>
+                </Grid>
 
                   <Grid container spacing={0}>
                     <Grid item xs={4}></Grid>
