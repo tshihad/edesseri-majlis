fd196a6e7ea1f11c506a0da3387d17b29ad3239c
tshihad <tshihad9@gmail.com>
2020-02-15 23:27:00 +0530

    change amount in loan to int again'

--- app/models/loan.go
+++ app/models/loan.go
@@ -8,8 +8,8 @@ import (
 type Loan struct {
 	ID                int       `json:"id,omit_empty" gorm:"primary_key"`
 	MemberID          string    `json:"member_id" gorm:"column:member_id"`
-	RequestAmount     string       `json:"request_amount" gorm:"column:request_amount"`
-	Installment       string       `json:"installment" gorm:"column:installment"`
+	RequestAmount     int       `json:"request_amount" gorm:"column:request_amount"`
+	Installment       int       `json:"installment" gorm:"column:installment"`
 	Purpose           string    `json:"purpose" gorm:"column:purpose"`
 	RequsetDate       time.Time `json:"request_date" gorm:"column:requset_date"`
 	GuarenterMemberID string    `json:"g_member_id" gorm:"column:g_member_id"`
